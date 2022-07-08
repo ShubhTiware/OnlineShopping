21e25edf69e3b8442bf7a191432cffb2118e0e73
ShubhTiware <108928514+ShubhTiware@users.noreply.github.com>
2022-07-08 15:59:36 +0530

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    public class Account
+    {
+        //Auto property
+        public  int Id { get; set; }
+        public double Balance { get; set; }
+        public double InterestRate { get; set; }
+
+        public Account()
+        {
+            Balance = 0;
+        }
+        public Account(double balance)
+        {
+            Random random = new Random(); 
+            this.Id = random.Next();
+            this.Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            this.Balance -= amount;
+            if (this.Balance <= 5000)
+            {
+                throw new Exception("Insufficient Balance");
+            }
+        }
+        public void Deposit(double amount)
+        {
+            this.Balance += amount;
+            if (this.Balance >= 250000)
+            {
+                throw new Exception("Balance Overflow");
+            }
+        }
+
+    }
+}

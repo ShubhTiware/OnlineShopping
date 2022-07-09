772a1cc315273db01365666407e44664577e1fff
SuhasKajawe <suhas.kajawe1008@gmail.com>
2022-07-09 14:44:53 +0530

    add hr class

---
+++ HrManager.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrManager
+{
+    public class HrManager : Account
+    {
+        //Auto property
+
+        public double sal { get; set; }
+
+       public HrManager():base()
+        {
+
+        }
+
+    }
+}
